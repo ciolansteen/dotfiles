d97e2f2dd9daeb6b4ca817aad92a16c618f42463
ciolansteen <ciolansteen@gmail.com>
2019-03-03 23:46:29 +0200

    Initial commit

---
+++ profile.sh
@@ -0,0 +1,129 @@
+# vim: filetype=sh
+# # # # # # # # # # # # # # # # # # # # #
+#    .  .  .  .  .  .  .  .  .  .  .    #      
+#   .  .  _  .  _  .  .  .|`|  .  .     #
+#    .  o`.\,=./.`o .  . _|_|_  .  .    #
+#   .  .  :(o o):  .  .  (o)o) .  .     #
+#    --ooO--(_)--Ooo-ooO--(_)--Ooo--    #
+#    ╔═╗╦╔═╗╦  ╔═╗╔╗╔╔═╗╔╦╗╔═╗╔═╗╔╗╔    #
+#    ║  ║║ ║║  ╠═╣║║║╚═╗ ║ ║╣ ║╣ ║║║    #
+#    ╚═╝╩╚═╝╩═╝╩ ╩╝╚╝╚═╝ ╩ ╚═╝╚═╝╝╚╝`s  #
+#            .profile file              #
+#                                       #
+# # # # # # # # # # # # # # # # # # # # #
+#
+# profile_CLN_RC ver=1.0
+#
+
+### Sources
+include (){
+    [[ -f "$1" ]] && source "$1"
+}
+
+include $HOME/.profilesen
+
+### History settings
+export HISTCONTROL=ignoreboth     # ignoredups:ignorespace
+
+### Alliases 
+## Usual commands overrides
+# cd & ls
+cdls(){
+    cd $1;
+    ls --color=tty -ah;
+}
+alias cd="cdls"
+
+# Maintainance
+
+
+
+# Miscellaneous
+alias plasmarestart='kquitapp5 plasmashell && sleep 1 && kstart5 plasmashell'
+alias locate='sudo updatedb && sudo locate -i'
+alias netflix='qutebrowser :open netflix.com &'
+alias :q='exit'
+alias pigrep='grep -Pi --color'
+alias ssystemctl='sudo systemctl'
+alias netrestart='sudo systemctl restart NetworkManager'
+alias mouserestart='sudo modprobe -r psmouse && sudo modprobe psmouse'
+alias touchpad_disable='xinput disable SynPS/2\ Synaptics\ TouchPad'
+alias touchpad_enable='xinput enable SynPS/2\ Synaptics\ TouchPad'
+alias trackpoint_disable='xinput disable TPPS/2\ IBM\ TrackPoint'
+alias trackpoint_enable='xinput enable TPPS/2\ IBM\ TrackPoint'
+alias reboot='sudo reboot'
+alias testasmtp='wine WinPrograms/Program\ Files\ \(x86\)/TestaSmtp.exe &'
+alias dosconf='vim ~/.dosbox/dosbox-0.74.conf'
+alias open='xdg-open'
+#alias vim='nvim'
+alias svim='sudo -E vim'
+alias www='cd ~/Devel/www'
+alias edp144='xrandr --output eDP-1-1 --mode 1920x1080 -r 60 && xrandr --output eDP-1-1 --mode 1920x1080 -r 144'
+
+# Fan control
+alias fan_auto='sudo echo level auto | sudo tee /proc/acpi/ibm/fan'
+alias fan_off='sudo echo level 0 | sudo tee /proc/acpi/ibm/fan'
+alias fan_1='sudo echo level 1 | sudo tee /proc/acpi/ibm/fan'
+alias fan_2='sudo echo level 2 | sudo tee /proc/acpi/ibm/fan'
+alias fan_3='sudo echo level 3 | sudo tee /proc/acpi/ibm/fan'
+alias fan_4='sudo echo level 4 | sudo tee /proc/acpi/ibm/fan'
+alias fan_5='sudo echo level 5 | sudo tee /proc/acpi/ibm/fan'
+alias fan_6='sudo echo level 6 | sudo tee /proc/acpi/ibm/fan'
+alias fan_7='sudo echo level 7 | sudo tee /proc/acpi/ibm/fan'
+
+# My servers
+alias webdev_start='sudo mount --bind ~/Devel/www /srv/http && sudo systemctl start httpd.service && sudo mount --bind ~/Devel/mySQL /var/lib/mysql && sudo systemctl start mysqld.service'
+alias webdev_stop='sudo systemctl stop httpd.service && sudo umount -l /srv/http && sudo systemctl stop mysqld.service && sudo umount -l /var/lib/mysql'
+
+# My VMs
+alias windows='VBoxManage startvm "Windows8.1"'
+
+# Pacman Aliasses
+alias yupdate='yay -Syyu && pkcon refresh'
+alias pacman='sudo pacman'
+alias pacinst='sudo pacman -S'
+alias pacsearch='sudo pacman -Ss'
+alias pacrmorphans='sudo pacman -Rns $(pacman -Qtdq)'
+alias pacmirrorefresh='sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist'
+alias paclistmyinstalls='expac -H M "%011m\t%-20n\t%10d" $(comm -23 <(pacman -Qqen | sort) <(pacman -Qqg base base-devel | sort)) | sort -n'
+
+alias monitor_birou_on='xrandr --addmode DP-1 1680x1050 && xrandr --output DP-1 --mode 1680x1050 && xrandr --output DP-1 --pos -1920x0'
+alias monitor_birou_off='xrandr --output DP-1 --off'
+
+alias cpumonitor='watch -n0.1 "grep \"cpu MHz\" /proc/cpuinfo && echo \"\" && sensors"'
+alias kwincompON='qdbus org.kde.KWin /Compositor resume'
+alias kwincompOFF='qdbus org.kde.KWin /Compositor suspend'
+
+# MC Trash Support
+alias mc='LD_PRELOAD=/usr/lib/libtrash.so mc'
+
+alias clear='printf "\033c"'
+
+
+
+# Replace default tty with FbTerm/Tmux and connect to MasterConsole or create it 
+# if not in ssh session
+if command -v tmux>/dev/null; then # check if tmux is installed 
+    # Chech if executed from a TTY and *not* from Tmux
+    if [[ "$(tty)" =~ /dev/tty ]] && [[ ! "$TERM" =~ screen ]] && [ -z "$TMUX" ]; then
+        fbterm -- $(tmux new-session -A -s MasterConsole) 
+    fi
+fi
+
+#mc_tmux(){
+#    [[ -z $TMUX ]] && mc -c || TERM=xterm-256color mc -c
+#}
+#alias mc=mc_tmux
+
+. /usr/share/LS_COLORS/dircolors.sh
+
+
+# Set default editor
+if [[ -e /usr/bin/nvim ]]; then
+        EDITOR=nvim
+elif [[ -e /usr/bin/vim ]]; then
+        EDITOR=vim
+else
+        EDITOR=vi
+fi
+
---
+++ zsh/zshrc.sh
@@ -0,0 +1,191 @@
+### Ciolansteen's ZSHrc
+
+# Settings
+# Load .profile file
+[[ -e ~/.profile ]] && emulate sh -c 'source $HOME/.profile'
+# Set ZSH theme (for some reasoun it has to be set before sourcing oh-my-zsh.sh
+ZSH_THEME="funky"
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+HIST_STAMPS="%d/%m/%y %k:%M:%S"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Default plugins are located in ~/.oh-my-zsh/plugins/*)
+# Custom plugins are located in ~/.oh-my-zsh/custom/plugins/
+# plugins=(rails git textmate ruby lighthouse)
+plugins=(
+    git
+    vi-mode
+    history-substring-search
+)
+
+source $ZSH/oh-my-zsh.sh
+
+### Look and feel
+# Use GNU dircolors
+if [ -f ~/.dircolors ]; then
+   eval `dircolors ~/.dircolors`
+fi
+
+# Zsh Syntax Highlighting
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Zsh Autosugestions
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+## VimMode for ZSH
+bindkey -v 
+## Show Vim NORMAL/INSERT Status
+function insert-mode () { echo "%{$fg_bold[yellow]$bg[#]%}-- INSERT -- %{$reset_color%}" }
+function normal-mode () { echo "%{$fg_bold[white]$bg[#]%}-- NORMAL -- %{$reset_color%}" }
+
+
+function set-prompt () {
+    case ${KEYMAP} in
+      (vicmd)      VI_MODE="$(normal-mode)" ;;
+      (main|viins) VI_MODE="$(insert-mode)" ;;
+      (*)          VI_MODE="$(insert-mode)" ;;
+    esac
+    RPS1="$VI_MODE"
+    PS1="$PS1"
+}
+
+
+function zle-line-init zle-keymap-select {
+    if [ $KEYMAP = vicmd ]; then
+        # -- NORMAL -- mode block cursor 
+        echo -ne "\e[2 q"
+    else
+        #  -- INSERT -- mode underscore cursor
+        echo -ne "\e[4 q"
+    fi
+    set-prompt
+    zle reset-prompt
+}
+
+preexec () { echo -ne '\e[5 q' }
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+## Saner KEYTIMEOUT (default = 40) to Kill <ESC> / I lag when switching -- INSERT -- / -- NORMAL -- in VimMode
+export KEYTIMEOUT=5
+#vimInsMode="%{$fg_bold[yellow]$bg[#]%}-- INSERT --%{$reset_color%}"
+#vimNormMode="%{$fg_bold[white]$bg[#]%}-- NORMAL --%{$reset_color%}"
+#
+#function zle-line-init zle-keymap-select {
+#    RPS1="${${KEYMAP/vicmd/${vimNormMode} }/(main|viins)/${vimInsMode} }"
+#    RPS2=$RPS1
+#    zle reset-prompt
+#}
+#
+
+## Don't use VimMode when inside MC to avoid mc hanging in ""-- NORMAL --" mode. 
+#if ! ps $PPID |grep mc; then
+#    bindkey -v 
+#    zle -N zle-line-init
+#    zle -N zle-keymap-select
+#fi
+
+# Prevent keeping Right PS info on that line after <CR>
+setopt transientrprompt
+
+# for linux console and RH/Debian xterm
+
+# SpecialKeys override to work with Vi-Mode
+if [[ "${terminfo[khome]}" != "" ]]; then
+  bindkey -M viins "\e[1~" beginning-of-line      # [Home] - Go to beginning of line
+  bindkey -M vicmd "\e[1~" beginning-of-line
+fi
+
+if [[ "${terminfo[kend]}" != "" ]]; then
+  bindkey -M viins "\e[4~" end-of-line      	   # [End] - Go to end of line
+  bindkey -M vicmd "\e[4~" end-of-line
+fi
+
+if [[ "${terminfo[kich1]}" != "" ]]; then
+  bindkey -M viins "${terminfo[kich1]}" overwrite-mode      	   # [Insert] - Insert Mode (Overwrite-mode) 
+  bindkey -M vicmd "${terminfo[kich1]}" overwrite-mode
+fi
+
+if [[ "${terminfo[kdch1]}" != "" ]]; then
+  bindkey -M viins "${terminfo[kdch1]}" delete-char      	   # [Delete] - Delete Character
+  bindkey -M vicmd "${terminfo[kdch1]}" delete-char
+fi
+
+if [[ "${terminfo[kpp]}" != "" ]]; then
+  bindkey -M viins "${terminfo[kpp]}" up-line-or-history     	   # [Page Up] - Up 1 Line or History Item 
+  bindkey -M vicmd "${terminfo[kpp]}" up-line-or-history
+fi
+
+if [[ "${terminfo[knp]}" != "" ]]; then
+  bindkey -M viins "${terminfo[knp]}" down-line-or-history      	   # [Page Down] - Down 1 Line or History Item 
+  bindkey -M vicmd "${terminfo[knp]}" down-line-or-history
+fi
+
+#bindkey -M viins "^[[A" history-beginning-search-backward      	   # [UP] - Search History Backward Based on Entered Letters 
+#bindkey -M vicmd "^[[A" history-beginning-search-backward 
+#
+#bindkey -M viins "^[[B" history-beginning-search-forward           # [DOWN] - Search History Forward Based on Entered Letters 
+#bindkey -M vicmd "^[[B" history-beginning-search-forward 
+#
+
+
+bindkey -M viins "^[[A" history-substring-search-up      	   # [UP] - Search History Backward Based on Entered Letters 
+bindkey -M vicmd "^[[A" history-substring-search-up 
+
+bindkey -M viins "^[[B" history-substring-search-down 
+         # [DOWN] - Search History Forward Based on Entered Letters 
+bindkey -M vicmd "^[[B" history-beginning-search-down 
+
+# Other useful Vim Like Overrides
+bindkey -M vicmd 'u' undo 										# Undo in -- NORMAL -- Mode
+bindkey -M vicmd '^r' redo 										# Redo in -- NORMAL -- Mode
+bindkey -M vicmd '/' history-incremental-search-backward        # Use / for Search
+
+# END Zsh Vim Mode
+##
+
+# Bindings for Xterm Home/End/Insert keys when not in Vi Mode
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[2~" overwrite-mode
+
+unset zle_bracketed_paste
+
+
+
+
+
+
+
+
+
+
+
+
+
