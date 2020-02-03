1aeed4ded7033d238e647fe25fa52a7c2cef9ad1
Ionut Adrian Ciolan <ciolansteen@gmail.com>
2020-02-03 10:07:12 +0200

    deleted bin

--- bin/interrup_remapping_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for i in $(dmesg | grep ecap | awk '{print $NF}'); do
-  if [ $(( (0x$i & 0xf) >> 3 )) -ne 1 ]; then
-    echo "Interrupt remapping not supported"
-    exit 1
-  fi
-done
--- bin/lattedockclean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat /dev/null > $HOME/.config/latte/.multiple-layouts_hidden.layout.latte
